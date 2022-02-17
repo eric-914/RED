33895014092e6df9834dcfa77df041bf470882a3
Eric Hood <Eric.Hood@ansira.com>
2022-02-17 11:49:08 -0600

    All services now have a tab

---
+++ RenderEngineDesktop/Controls/ZipFileFields.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Data;
+
+namespace RenderEngineDesktop.Controls
+{
+    public partial class ZipFileFields 
+    {
+        public ZipFileFields()
+        {
+            InitializeComponent();
+        }
+
+        public Binding Context
+        {
+            set => SetBinding(DataContextProperty, value);
+        }
+    }
+}
