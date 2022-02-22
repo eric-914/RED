a816a5e4bfb6e2ccbbf96549adcec2f83f1ed2b7
Eric Hood <Eric.Hood@ansira.com>
2022-02-21 18:42:41 -0600

    Played around with styling.

---
+++ RenderEngineDesktop/Support/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace RenderEngineDesktop.Support
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Execute an update a (vertical) scroll-viewer for a mouse-wheel event
+        /// </summary>
+        public static void VScroll(this ScrollViewer sender, MouseWheelEventArgs e)
+        {
+            sender.ScrollToVerticalOffset(sender.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+    }
+}
--- RenderEngineDesktop/Views/ListEnumeratedFonts/ListEnumeratedFontsView.xaml.cs
+++ RenderEngineDesktop/Views/ListEnumeratedFonts/ListEnumeratedFontsView.xaml.cs
@@ -1,15 +1,20 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
+using RenderEngineDesktop.Support;
 
 namespace RenderEngineDesktop.Views.ListEnumeratedFonts
 {
-    /// <summary>
-    /// Interaction logic for ListEnumeratedFontsView.xaml
-    /// </summary>
-    public partial class ListEnumeratedFontsView : UserControl
+    public partial class ListEnumeratedFontsView
     {
         public ListEnumeratedFontsView()
         {
             InitializeComponent();
+        }
+
+        //--Without this, the mouse wheel only works while over the scrollbar.
+        private void UIElement_OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ((ScrollViewer)sender).VScroll(e);
         }
     }
 }
